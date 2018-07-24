7383688323db71d9f63b7ea31d2f15336d4147e5
오세진 <jinioh88@gmail.com>
2018-07-24 11:07:14 +0900

    포워드 / 인클루딩

--- step00/src/spms/servlets/MemberListServlet.java
+++ step00/src/spms/servlets/MemberListServlet.java
@@ -1,12 +1,13 @@
 package spms.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import spms.vo.Member;
+
+// UI 출력 코드를 제거하고, UI 생성 및 출력을 JSP에게 위임한다.
 @WebServlet("/member/list")
 public class MemberListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -40,24 +44,32 @@ public class MemberListServlet extends HttpServlet {
 					" ORDER BY MNO ASC");
 			
 			response.setContentType("text/html; charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.println("<html><head><title>회원목록</title></head>");
-			out.println("<body><h1>회원목록</h1>");
-			out.println("<p><a href='add'>신규 회원</a></p>");
-			while(rs.next()) {
-				out.println(
-					rs.getInt("MNO") + "," +
-					"<a href='update?no=" + rs.getInt("MNO") + "'>" +
-					rs.getString("MNAME") + "</a>," +
-					rs.getString("EMAIL") + "," + 
-					rs.getDate("CRE_DATE") + 
-					"<a href='delete?no=" + rs.getInt("MNO") + 
-					"'>[삭제]</a><br>");
-			}
-			out.println("</body></html>");
-		} catch (Exception e) {
-			throw new ServletException(e);
+			ArrayList<Member> members = new ArrayList<Member>();
 			
+			// 데이터베이스에서 회원 정보를 가져와 Member에 담는다.
+			// 그리고 Member객체를 ArrayList에 추가한다.
+			while(rs.next()) {
+				members.add(new Member()
+							.setNo(rs.getInt("MNO"))
+							.setName(rs.getString("MNAME"))
+							.setEmail(rs.getString("EMAIL"))
+							.setCreatedDate(rs.getDate("CRE_DATE"))	);
+			}
+			
+			// request에 회원 목록 데이터 보관한다.
+			request.setAttribute("members", members);
+			
+			// JSP로 출력을 위임한다.
+			RequestDispatcher rd = request.getRequestDispatcher(
+					"/member/MemberList.jsp");
+			rd.include(request, response);
+			
+		} catch (Exception e) {
+			request.setAttribute("error", e);
+			RequestDispatcher rd = request.getRequestDispatcher(
+					"Error.jsp");
+			rd.forward(request, response);
+
 		} finally {
 			try {if (rs != null) rs.close();} catch(Exception e) {}
 			try {if (stmt != null) stmt.close();} catch(Exception e) {}
